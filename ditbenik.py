7d1e82b0d9e385fe3f22a6470433dd51daed2491
Abdi <29274@ma-web.nl>
2021-10-06 23:39:13 +0200

    welkom

---
+++ ditbenik.py
@@ -0,0 +1,162 @@
+import time
+import os
+import sys
+import random
+from typing import NoReturn
+
+print("Hello You!")
+print("Ik ben Abdi Abdikarim \n")
+print("Ik woon in Haarlem")
+print("Type je Naam Hier: ")
+x = input()
+print("Hallo " +  x , " aangenaam \n")
+
+print("ik ben een Nieuwkomer op het Mediacollege Amsterdam \nDoor aantal vragen over mij te beantwoorden leer je mij beter kennen. \n\n")
+
+while True:
+        print("Kan je raden wat mijn afkomst is? \n\n")
+
+        print("A. Somaliër.")
+        print("B. Nederland")
+
+        answer = input()
+
+        if answer == "A" or "a":
+                print("Ja ik ben een Somaliër.\n")
+                break
+        elif answer == "B" or "b":
+                print("Nee ik ben opgegroeid in Nederland maar ik ben geen Nederlander.")
+                break
+                
+                
+                
+while True:
+        print("Voordat ik naar het MBO op het MediaCollege Amsterdam kwam zat ik op het? \n\n")
+
+        print("A. NOVA college Haarlem entree.")
+        print("B. ROC NOVA college Beverwijk ")
+        print("C. VMBO De Oosterhout Haarlem.")
+
+
+        answer1 = input()
+
+        if answer1 == "A" or "a":
+                        print("Nova college Haarlem was mijn eerste MBO school maar dat niet waar voor het Media college zat ik op het ROC Nova college Beverwijk.")
+                        break
+        elif answer1 == "B" or "b":
+                        print("voordat ik op het Media college kwam zat ik op het ROC nova college Beverwijk.\n")
+                        break
+        elif answer1 == "C" or "c":
+                        print("zat ik op het VMBO oosterhout.")
+                        break
+
+
+while True:
+        print("wat is mijn leeftijd volgens jou?.")
+
+        print("A. ik ben 20 jaar jong.")
+        print("B. ik ben 16 jaar jong")
+        print("C. ik weet het niet joh.")
+
+        answer2 = input()
+        if answer2 == "A" or "a":
+                print("Ja ik ben 20 jaar jong.\n")
+                break
+        elif answer2 == "B" or  "b":
+                print("Nee ik ben 20 jaar jong geen 12.")
+                break         
+        elif answer2 == "C" or   "c":
+                print("oowh wat jammer maar ik ben 20 jaar oud.") 
+                break
+
+
+while True:
+        print("wat zijn mijn favoriete kleur?")
+
+        print("A. groen")
+        print("B. blauw")
+        print("C. zwart")
+
+
+        answer3 = input()
+
+        if answer3 == "A" or "a":
+                print("nee groen is niet mijn favoriete kleur.")
+                break
+        elif answer3 == "B" or "b":
+                print("ja blauw is mijn favoriete kleur.")
+                break
+        elif answer3 == "C" or "c":
+                print("Ik vind zwart kleur mooi maar het is niet mijn favoriete kleur.")
+                break
+
+                
+while True:
+        print("wat is mijn favoriete voetbal club volgens jou?\n")
+
+        print("A. chelsea")
+        print("B. manchester united")
+        print("C. Real madrid")
+
+        answer4 = input()
+
+        if answer4 == "A" or "a":
+                print("nee chelsea is niet mijn favoriete club dat is manchester united.")
+                break
+        elif answer4 == "B" or "b":
+                print("Ja manchester united is mijn favoriete club.")
+                break
+        elif answer4 == "C" or "c":
+                print("nooit real madrid always mancherster united")
+                break
+
+
+
+while True:
+        print("wat is jouw leeftijd?\n")
+
+        print("A. ik bent 16 jaar jong")
+        print("B. ik bent 18 jaar jong")
+        print("C. anders.")
+
+        answer5 = input()
+        
+        if answer5 == "A" or "a":
+                print(" niet mooi zeg hoi hoi.")
+                break
+        elif answer5 == "B" or "b":
+                print("je bent fucking jong man.")
+                break
+        elif answer5 == "C" or "c":
+                print("je hebt mooi leeftijd.")
+
+                break
+
+while True:
+        print("wat zijn jou favoriete eten?\n")
+
+        y = input("Type hier:")
+        print(" jummy wat een gerecht hey  lekker " + y )
+
+        print("wat is jou afkomt?\n")
+        u = input("Type hier:")
+        print(" waaw ik had het niet van je verwacht ouwe\nmooi afkomst. " + x + " ik wist niet dat jij " + u + " was")
+
+        print("laatste vraag.")
+        print("waar kom je vadaan? ")
+        w = input("Type hier: ")
+        print(" wat een mooi stad man. " +  x, +   w  +  " prachtig")
+        
+        break
+
+
+        
+
+
+
+
+
+
+
+
+
---
+++ ditbenikuitgebreid.py
@@ -0,0 +1,48 @@
+import datetime
+import os
+import sys
+
+
+print("Hallo mijn naam is Abdi Abdikarim.\n")
+
+print("Hoe heet jij?: ")
+
+x = input()
+
+print("hello " + x + " It's nice to meet you.\n")
+
+print("ik ben een Nieuwkomer op het Mediacollege Amsterdam\n\n")
+
+# vraag 1 ?
+while True:
+    print("Is this the one the list?")
+
+
+    print("A. Yeah I guess." + x)
+    print("B. I do no I lost the list D.")
+    print("C. I do have it.")
+
+    answer1 = input()
+
+    if answer1 ==  "A" or "a":
+        print("What do you mean you guess? It either is or it isn’t.")
+    elif answer1 == "B" or "a":
+        print("You realize how much we’re going to get our asses chewed out because of your fuck up? I’m sick of paying for your mistakes.")
+    elif answer1 == "C" or "a":
+        print("That’s just great thank God.\n")
+
+while True:
+    print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
